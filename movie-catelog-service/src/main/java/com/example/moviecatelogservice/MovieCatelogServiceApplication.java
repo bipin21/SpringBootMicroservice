4c7e56f2a38751b9930749b60ca9a7453fc1fbed
Bipin Karki <bipinkarki@Bipins-MacBook-Pro.local>
2021-09-27 16:40:59 -0500

    push to docker

--- movie-catelog-service/src/main/java/com/example/moviecatelogservice/MovieCatelogServiceApplication.java
+++ movie-catelog-service/src/main/java/com/example/moviecatelogservice/MovieCatelogServiceApplication.java
@@ -2,9 +2,17 @@ package com.example.moviecatelogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MovieCatelogServiceApplication {
+	
+	@GetMapping("/message")
+	public String message(){
+		return "welcome message";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatelogServiceApplication.class, args);
---
+++ movie-catelog-service/src/main/java/com/example/moviecatelogservice/model/Movie.java
@@ -0,0 +1,8 @@
+package com.example.moviecatelogservice.model;
+
+public class Movie {
+
+    private long movieId;
+    private String name;
+    private String description;
+}
